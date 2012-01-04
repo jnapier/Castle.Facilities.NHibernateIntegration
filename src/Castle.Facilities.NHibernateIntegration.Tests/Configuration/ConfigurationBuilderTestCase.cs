546427cc933bd7e4a94d2fde0db92dd517855160
hconceicao <henry.conceicao@gmail.com>
2012-01-04 02:37:33 -0200

    updated license header

--- src/Castle.Facilities.NHibernateIntegration.Tests/Configuration/ConfigurationBuilderTestCase.cs
+++ src/Castle.Facilities.NHibernateIntegration.Tests/Configuration/ConfigurationBuilderTestCase.cs
@@ -1,6 +1,5 @@
 #region License
-
-//  Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+//  Copyright 2004-2012 Castle Project - http://www.castleproject.org/
 //  
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -13,10 +12,8 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-// 
-
+//  
 #endregion
-
 namespace Castle.Facilities.NHibernateIntegration.Tests.Configuration
 {
 	using Common;
